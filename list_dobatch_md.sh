855559893ad13db8171b546baea92d18d62a31a8
Robert Eustace Novak <sailnfool@gmail.com>
2020-02-05 16:50:22 -0800

    Handle large batches of ior and mdtests

--- list_dobatch_md.sh
+++ list_dobatch_md.sh
@@ -3,55 +3,52 @@
 # Author: Robert E. Novak
 # email: novak5@llnl.gov, sailnfool@gmail.com
 #
-# Run ior based on lists of runs, lists of file systems and lists of
-# processes provided as arguments.  E.G.
+# Run a batch of mdtest benchmarks
 #
-# dobatch_ior_list -f 1 -r 1 -p 1
 ########################################################################
 source func.global
 source func.errecho
 
-ior_filesystemlistprefix=ior.filesystems
-ior_runnerlistprefix=ior.runner
-ior_processlistprefix=ior
+md_filesystemlistprefix=md.filesystems
+md_runnerlistprefix=md.runner
+md_processlistprefix=md
 
-ior_runnerlist="ior_runner -x mi25 -p10"
-ior_runnerlistfile=""
+md_runnerlist="md_runner -x mi25 -p10"
+md_runnerlistfile=""
 
-ior_filesystemlist="/p/lustre3"
-ior_filesystemlistfile=""
+md_filesystemlist="/p/lustre3"
+md_filesystemlistfile=""
 
-ior_processlist=10
-ior_processlistfile=""
+md_processlist=10
+md_processlist=""
 
 debug=0
 DEBUGSETX=6
 DEBUGNOEXECUTE=9
-
 USAGE="${0##*/} [-[hv]] -r <list?.txt> -f <list?.txt> -p <list?.txt>\r\n
 \t-h\t\tPrint this help information\r\n
 \t-v\t\tTurn on verbose mode (works for -h: ${0##*/} -v -h)\r\n
-\t-f\t<#>\tretrieves ${ior_filesystemlistprefix}.list<#>.txt for\r\n
+\t-f\t<#>\tretrieves ${md_filesystemlistprefix}.list<#>.txt for\r\n
 \t\t\ta list of the filesystems that will be tested.  These should\r\n
 \t\t\tall be mpi filesystems.\r\n
-\t-r\t<#>\tretrieves ${ior_runnerlistprefix}.list<#>.txt for a\r\n
+\t-r\t<#>\tretrieves ${md_runnerlistprefix}.list<#>.txt for a\r\n
 \t\t\tlist of the iorrunner commands (with options) that will\r\n
 \t\t\tbe run as tests.\r\n
-\t-p\t<#>\tretrieves ${ior_processlistprefix}.list<#>.txt for a\r\n
+\t-p\t<#>\tretrieves ${md_processlistprefix}.list<#>.txt for a\r\n
 \t\t\tlist of the number of processes that will be requested\r\n
 \t\t\twhen running iorrunner.  Note that number of processes\r\n
 \t\t\tand -p <percentage> of nodes to processes.  Slightly\r\n
 \t\t\tconfusing, see -vh\r\n"
-VERBOSE_USAGE="${0##*/} Make sure you see ior_runner -h and -vh\r\n
-\t\tThe set of all files for filesystems, ior_runner commands\r\n
+VERBOSE_USAGE="${0##*/} Make sure you see md_runner -h and -vh\r\n
+\t\tThe set of all files for filesystems, md_runner commands\r\n
 \t\tand process lists are found by 'ls ior.\*.list\*.txt'\r\n
 \t\tthis is highly useful if you want to perform\r\n
 \t\tcomparison of lustre1 to lustre3 or any other\r\n
 \t\tsets of filesystems.\r\n
 \r\n
-\t\tDefault runner list = ${ior_runnerlist}\r\n
-\t\tDefault filesystem list = ${ior_filesystemlist}\r\n
-\t\tDefault process list = ${ior_processlist}\r\n"
+\t\tDefault runner list = ${md_runnerlist}\r\n
+\t\tDefault filesystem list = ${md_filesystemlist}\r\n
+\t\tDefault process list = ${md_processlist}\r\n"
 
 list_optionargs="hvr:f:p:d:"
 
@@ -77,28 +74,28 @@ do
 			;;
 		f)
 			num=${OPTARG}
-			ior_filesystemlistfile="${ior_filesystemlistprefix}.list${num}.txt"
-			if [ ! -r ${ior_filesystemlistfile} ]
+			md_filesystemlistfile="${md_filesystemlistprefix}.list${num}.txt"
+			if [ ! -r ${md_filesystemlistfile} ]
 			then
-				errecho ${0##*/} ${LINENO} "file ${ior_filesystemlistfile} not found"
+				errecho ${0##*/} ${LINENO} "file ${md_filesystemlistfile} not found"
 				exit 1
 			fi
 			;;
 		p)
 			num=${OPTARG}
-			ior_processlistfile="${ior_processlistprefix}.list${num}.txt"
-			if [ ! -r ${ior_processlistfile} ]
+			md_processlistfile="${md_processlistprefix}.list${num}.txt"
+			if [ ! -r ${md_processlistfile} ]
 			then
-				errecho ${0##*/} ${LINENO} "file ${ior_processlistfile} not found"
+				errecho ${0##*/} ${LINENO} "file ${md_processlistfile} not found"
 				exit 1
 			fi
 			;;
 		r)
 			num=${OPTARG}
-			ior_runnerlistfile="${ior_runnerlistprefix}.list${num}.txt"
-			if [ ! -r ${ior_runnerlistfile} ]
+			md_runnerlistfile="${md_runnerlistprefix}.list${num}.txt"
+			if [ ! -r ${md_runnerlistfile} ]
 			then
-				errecho ${0##*/} ${LINENO} "file ${ior_runnerlistfile} not found"
+				errecho ${0##*/} ${LINENO} "file ${md_runnerlistfile} not found"
 				exit 1
 			fi
 			;;
@@ -117,16 +114,22 @@ shift $((OPTIND-1))
 mkdir -p ${IOR_TESTDIR} ${IOR_ETCDIR}
 
 ####################
-# Create a lock file so that two different scripts don't update
-# the test number
+# Create a lock file so that two different scripts don't update the test
+# number
 ####################
 echo $(func_getlock) | sed '/^$/d' >> ${LOCKERRS}
+
+####################
+# Use a file to keep track of the number of tests that have been run by this 
+# script against the executable.
+####################
+iorbatchnumberfile=${ioretcdir}/IOR.BATCHNUMBER
 
 ####################
 # if it does not exist, initialize it with a zero value
 ####################
 
-if [ ! -r ${IOR_BATCHNUMBERFILE} ]
+if [ ! -r ${IOR_BATCHNUMBER} ]
 then
 	echo 0 > ${IOR_BATCHNUMBERFILE}
 fi
@@ -140,55 +143,59 @@ iorbatchnumber=$(cat ${IOR_BATCHNUMBERFILE})
 # bump the test number and stuff it back in the file.
 ####################
 ((++iorbatchnumber))
-echo ${iorbatchnumber} > ${IOR_BATCHNUMBERFILE}
+echo ${iorbatchnumber} > ${IOR_BATCHNUMBER}
 
+####################
+# Now we can release the lock 
+####################
 echo $(func_releaselock) | sed '/^$/d' >> ${LOCKERRS}
 
 ####################
-# retrieve the current batch number and stuff it in a test string for
+# retrieve the current test number and stuff it in a test string for
 # identifying the results directory
 ####################
-iorbatchstring="${USER}-BATCH-IOR-$(printf '%04d' ${iorbatchnumber})"
+mdbatchstring="${USER}-BATCH-MD-$(printf '%04d' ${iorbatchnumber})"
 
-export iorbatchstring
+export mdbatchstring
 
-if [ ! -z "${ior_processlistfile}" ]
+
+if [ ! -z "${md_processlistfile}" ]
 then
-	ior_processlist=""
-	for procnum in $(cat ${ior_processlistfile})
+	md_processlist=""
+	for procnum in $(cat ${md_processlistfile})
 	do
-		ior_processlist="${ior_processlist} ${procnum}"
+		md_processlist="${md_processlist} ${procnum}"
 	done
 fi
-if [ ! -z "${ior_filesystemlistfile}" ]
+if [ ! -z "${md_filesystemlistfile}" ]
 then
-	ior_filesystemlist=""
-	for filesystem in $(cat ${ior_filesystemlistfile})
+	md_filesystemlist=""
+	for filesystem in $(cat ${md_filesystemlistfile})
 	do
-		ior_filesystemlist="${ior_filesystemlist} ${filesystem}"
+		md_filesystemlist="${md_filesystemlist} ${filesystem}"
 	done
 fi
-if [ ! -z "${ior_runnerlistfile}" ]
+if [ ! -z "${md_runnerlistfile}" ]
 then
 	while read -r command
 	do
-		for filesystem in ${ior_filesystemlist}
+		for filesystem in ${md_filesystemlist}
 		do
-			echo "${command} -f ${filesystem} ${ior_processlist}"
+			echo "${command} -f ${filesystem} ${md_processlist}"
 			if [ $debug -lt ${DEBUGNOEXECUTE} ]
 			then
-				${command} -f ${filesystem} ${ior_processlist}
+				${command} -f ${filesystem} ${md_processlist}
 			fi
 		done
-	done < ${ior_runnerlistfile}
+	done < ${md_runnerlistfile}
 else
 	command=${runnerlist}
-	for filesystem in ${ior_filesystemlist}
+	for filesystem in ${md_filesystemlist}
 	do
-		echo "${command} -f ${filesystem} ${ior_processlist}"
+		echo "${command} -f ${filesystem} ${md_processlist}"
 		if [ $debug -lt ${DEBUGNOEXECUTE} ]
 		then
-			${command} -f ${filesystem} ${ior_processlist}
+			${command} -f ${filesystem} ${md_processlist}
 		fi
 	done
 fi
--- makefile
+++ makefile
@@ -7,7 +7,7 @@
 INSTALL = extract_ior \
 					extract_md \
 					ior_runner dobatch_ior list_dobatch_ior \
-					md_runner dobatch_md \
+					md_runner dobatch_md list_dobatch_md\
 					func.logger \
 					func.global
 
